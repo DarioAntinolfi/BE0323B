940c4aa71cdb83fce453a42f0b079d16755b2d00
DarioAntinolfi <antinolfidario@gmail.com>
2023-10-04 15:56:37 +0200

    secondo commit
    secondo

--- Unit1/Settimana1/Giorno3/U1S1D3/src/SIM.java
+++ Unit1/Settimana1/Giorno3/U1S1D3/src/SIM.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class SIM {
     int numeroTelefonico;
     double creditoResiduo = 0;
@@ -7,6 +9,17 @@ public class SIM {
 
     public void costruttore (int a){
         this.numeroTelefonico = a;
+    }
+    public void stampaDati () {
+        System.out.println(numeroTelefonico);
+        System.out.println(creditoResiduo);
+        System.out.println(" ");
+        for (int i = 0; i < 5 ;i++){
+            System.out.println(this.elencoNomiChiamate[i]);
+            System.out.println(this.elencoNumeriChiamate[i]);
+            System.out.println(this.elencominutiChiamate[i]);
+            System.out.println(" ");
+        }
     }
 }
 
--- Unit1/Settimana1/Giorno3/U1S1D3/src/es2.java
+++ Unit1/Settimana1/Giorno3/U1S1D3/src/es2.java
@@ -1,5 +1,7 @@
 public class es2 {
         public static void main(String[] args) {
-
+                SIM Tonino = new SIM();
+                Tonino.costruttore(1234567890);
+                Tonino.stampaDati();
         }
 }
