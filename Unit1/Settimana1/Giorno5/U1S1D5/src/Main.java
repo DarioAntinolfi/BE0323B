3f7d1f369e2eafcf573148a919a470df9d94e563
DarioAntinolfi <antinolfidario@gmail.com>
2023-10-06 14:42:11 +0200

    play elemento
    creato il play elemento di array, manca da sistemare l'array

--- Unit1/Settimana1/Giorno5/U1S1D5/src/Main.java
+++ Unit1/Settimana1/Giorno5/U1S1D5/src/Main.java
@@ -10,54 +10,65 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        /*-Array-*/
         TipoElemento Elemento;
         String conversione = "Audio";
         String tipo = "";
         String nome = "";
         int durata = 0;
         ElementoMultimediale[] cronologia = new ElementoMultimediale[5];
-        for (int i = 0; i < 5; i++){
+        String[] nomi = {"uno", "due", "tre", "quattro","cinque"};
+        Scanner input = new Scanner(System.in);
+        Audio prova =new Audio("cane", 2);
+        for(int i = 0; i < 5; i++) {
+            System.out.println("Inserisci un tipo di Elemento");
+            conversione = input.nextLine();
+            Elemento = TipoElemento.valueOf(conversione);
+            System.out.println("Inserisci un tipo contenuto");
+            tipo = input.nextLine();
+            nomi[i] = tipo;
+            System.out.println("Inserisci un nome contenuto");
+            nome = input.nextLine();
+            System.out.println("Inserisci un durata contenuto");
+            durata = input.nextInt();
+
+            switch (Elemento) {
+                case Audio: {
+                    Audio nomi[i] = new Audio(nome, durata);
+                    System.out.println("Hai creato un Audio");
+                    break;
+                }
+                case Video: {
+                    Video nomi[i] = new Video(nome, durata);
+                    System.out.println("Hai creato un Audio");
+                    break;
+                }
+                case Immagine: {
+                    System.out.println("non poi creare immagini");
+                    i--;
+                    break;
+                }
+                default: {
+                    System.out.println("scelta non valida");
+                    i--;
+                    break;
+                }
+
+            }
+
             cronologia[4] = cronologia[3];
             cronologia[3] = cronologia[2];
             cronologia[2] = cronologia[1];
             cronologia[1] = cronologia[0];
-            cronologia[0] = ;
+            //cronologia[0] = nomi[i];
         }
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Inserisci un tipo di Elemento");
-        conversione = input.nextLine();
-        Elemento = TipoElemento.valueOf(conversione);
-        System.out.println("Inserisci un tipo contenuto");
-        tipo = input.nextLine();
-        System.out.println("Inserisci un nome contenuto");
-        nome = input.nextLine();
-        System.out.println("Inserisci un durata contenuto");
-        durata = input.nextInt();
+        System.out.println("Scegli cosa far eseguire (1 a 5)");
+        int cursore = input.nextInt() - 1;
+        cronologia[cursore].play();
 
-        switch (Elemento){
-            case Audio:{
-                Audio $tipo = new Audio(nome, durata);
-                break;
-            }
-            case Video:{
-                Video $tipo = new Video(nome, durata);
-                break;
-            }
-            case Immagine: {
-                System.out.println("non poi inserire immagini");
-            }
-            default:{
-                System.out.println("scelta non valida");
-                break;
-            }
 
-        }
-
-        Video cane = new Video("baldo", 4);
-        cronologia[0] = cane;
 
         input.close();
+
     }
 }
