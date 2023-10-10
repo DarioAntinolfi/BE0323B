534068efec05e5af9a3f4f9ec51ad6e18ef4e790
DarioAntinolfi <antinolfidario@gmail.com>
2023-10-10 16:58:57 +0200

    fine es 2
    es2

--- Unit1/Settimana2/Giorno2/U1S2D2/src/Es2/Es2.java
+++ Unit1/Settimana2/Giorno2/U1S2D2/src/Es2/Es2.java
@@ -18,8 +18,23 @@ public class Es2 {
         }
 
         System.out.println("liste al contrario");
-        
+        ArrayList<String> parole = new ArrayList<>();
+        System.out.println("Quante parole vuoi?");
+        int numeroParole = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < numeroParole; i++){
+            System.out.println("inserisci parola");
+            String parola = input.nextLine();
+            parole.add(parola);
+        }
+
+        System.out.println("Inversione");
+        ArrayList<String> paroleInverse = new ArrayList<>();
+        for (int i = 0; i < numeroParole; i++){
+            paroleInverse.add(parole.get(numeroParole - 1 - i));
+            System.out.println(parole.get(numeroParole - 1 - i));
+        }
 
         input.close();
     }
+
 }
