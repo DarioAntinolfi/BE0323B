fa0ed2de84df9e6c08509dba624b450dbaee28f5
DarioAntinolfi <antinolfidario@gmail.com>
2023-10-13 11:17:20 +0200

    Aggiunta di libri
    prima aggiunta

--- Unit1/Settimana2/Giorno5/U1S2D5/src/Main.java
+++ Unit1/Settimana2/Giorno5/U1S2D5/src/Main.java
@@ -1,12 +1,16 @@
 import Classi.Libri;
 import Classi.Riviste;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 import static Classi.periodicità.mensile;
 import static Classi.periodicità.settimanale;
 
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
 
         /*-Archivio di base-*/
         Libri libro1 = new Libri(1, "Aldo", 1995, 300, "Aldo Baglio", "Biografia");
@@ -16,6 +20,32 @@ public class Main {
         Riviste rivista2 = new Riviste(5, "Chi l'ha visto?", 2006, 48, mensile);
 
 
+        /*-Aggiungere Libri-*/
+        int indice = 6;
+        boolean aggiungereLibro = true;
+        String si = "si";
+        while (aggiungereLibro == true) {
+            System.out.println("Vuoi aggiungere un libro?");
+            String risposta = input.nextLine();
+            if (Objects.equals(risposta, si)){
+                System.out.println("Inserire un titolo");
+                String newTitolo = input.nextLine();
+                System.out.println("Inserire anno di publicazione");
+                int newAnnoDiPublicazione = Integer.parseInt(input.nextLine());
+                System.out.println("Inserire numero di pagine");
+                int newNumeroDiPagine = Integer.parseInt(input.nextLine());
+                System.out.println("Inserire un autore");
+                String newAutore = input.nextLine();
+                System.out.println("Inserire una categotia");
+                String newCategoria = input.nextLine();
+                Libri newLibro = new Libri(indice, newTitolo, newAnnoDiPublicazione, newNumeroDiPagine, newAutore, newCategoria);
+                indice++;
+
+            } else{
+                aggiungereLibro = false;
+            }
+
+        }
 
     }
 }
